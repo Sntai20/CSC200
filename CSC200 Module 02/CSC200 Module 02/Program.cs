ddb9464f2d2891de6b385e381ebf872e5f21f2fb
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-18 09:06:36 -0700

    Added class notes.

---
+++ CSC200 Module 02/CSC200 Module 02/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC200_Module_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rectangle myRectangle = new Rectangle();
+            ////myRectangle.width = 2;
+            ////myRectangle.height = 17;
+            //Console.WriteLine("Area = "+myRectangle.GetArea());
+            //Console.WriteLine(100);
+            //Console.WriteLine("MSSA");
+
+            //myRectangle.GetArea();
+
+            //////////////////////////////////////////////////////
+            int x = 10;
+            int y = 1;
+            Console.WriteLine("Before: x={0}, y={1}", x, y);
+            Test.Swap(ref x, ref y);
+            Console.WriteLine("After: x={0}, y={1}", x, y);
+
+            Test.Method(1,true, "MSSA");
+            Test.Method(1);
+            Test.Method(1, false);
+            //Test.Method(1, "MSSA");
+            Test.Method(1, c: "MSSA");
+
+            // int twice;
+            // int squared;
+            //Test.Method2(3, out twice, out squared);
+            Test.Method2(3, out int twice, out int squared);
+            Console.WriteLine(twice+ ", "+squared);
+
+            try
+            {
+                ///int[] values = { };
+                int[] values = {2,3,4,5,212,3,1 };
+                Console.WriteLine(Test.FindMax(values));
+
+                Debug.Assert(Test.FindMax(values) == 211);
+            }
+            //catch(ExecutionEngineException e) -- wrong type
+            //catch (IndexOutOfRangeException e) -- right type
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e) //-- the general type
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+            Console.WriteLine("ta da!");
+        }
+    }
+    class Test
+    {
+        public static int FindMax(int[] arr)
+        {
+            if (arr.Length > 0)
+            {
+                int max = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    if (arr[i] > max)
+                        max = arr[i];
+                }
+
+                return max;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("the array is empty!!!!!");
+                Console.WriteLine("you'll never see me!");
+            }
+        }
+        public static void Swap(ref int a, ref int b)
+        {
+            //Console.WriteLine("Before: a={0}, b={1}", a,b);
+            int tmp = a;
+            a = b;
+            b = tmp;
+            //Console.WriteLine("After: a={0}, b={1}", a, b);
+        }
+
+        public static void Method(int a, bool b=true, string c="MSSA")
+        {
+
+        }
+
+        public static void Method2(int a, out int twiceA, out int squaredA)
+        {
+            twiceA = 2 * a;
+            squaredA = a * a;
+        }
+    }
+
+
+    class Rectangle
+    {
+        //data (fields and properties)
+        public int height, width;
+
+        private void DummyMethod()
+        {
+
+        }
+
+        //methods (and event handlers ...)
+        public int GetArea()
+        {
+            DummyMethod();
+            return height * width;
+        }
+
+        //constructor
+        public Rectangle(int newHeight , int newWidth = 0)
+        {
+            height = newHeight;
+            width = newWidth;
+        }
+        
+
+        public Rectangle()
+        {
+            height = 0;
+            width = 0;
+        }
+
+
+    }
+
+}
---
+++ CSC200 Module01/CSC200 Module01/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC200_Module01
+{
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> mylist = new List<int>();
+
+
+            MyClass obj = new MyClass();
+            //obj.Z;
+
+            MyNamespace.Core.v2.MyClass obj2 = new MyNamespace.Core.v2.MyClass();
+            // obj2.
+
+            var x = 3.5;
+
+            StringBuilder myStr = new StringBuilder();
+
+            int i = 1;
+            Console.WriteLine(i++);
+            Console.WriteLine(i);
+
+            int yy = 7;
+            switch(yy)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("menu option 1");
+                        goto case 2;//break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("menu option 1");
+                        break;
+                    }
+            }
+
+            for(int p=0;p<10; p++)
+                Console.WriteLine(p);
+
+            //while(true)
+            //    Console.WriteLine("neverending");
+
+            //for(; ; )
+            //    Console.WriteLine("neverending");
+        }
+
+    }
+    
+    class MyClass
+    {
+        public String Z { get; set; }
+    }
+}
+
+namespace MyNamespace.Core.v2
+{
+    namespace NestedNamespace
+    {
+
+    }
+    class MyClass
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
